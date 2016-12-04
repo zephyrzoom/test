c42ab2ae50f194778d55b23cb954da6427f07e87
707 <707472783@qq.com>
2016-12-04 18:20:01 +0800

    add pages

--- catch.js
+++
@@ -1,8 +0,0 @@
-try {
-    undefined(); // illegal operation to force an exception!
-}
-catch (err) {
-    console.log( err ); // works!
-}
-
-console.log( err ); // ReferenceError: `err` not found
--- js_module/test.js
+++
@@ -1,9 +0,0 @@
-// import the entire "foo" and "bar" modules
-module foo from "foo";
-module bar from "bar";
-
-console.log(
-    bar.hello( "rhino" )
-); // Let me introduce: rhino
-
-foo.awesome(); // LET ME INTRODUCE: HIPPO
